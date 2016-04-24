f458dbe5f476e3779698811243f5ed756e925dcb
Ben Merckx <ben@codeurs.be>
2016-04-24 17:52:52 +0200

    Package db as resource, adds support for java

--- submit.sh
+++ submit.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 npm install mime-db --silent
-awk 'FNR==NR{s=(!s)?$0:s RS $0;next} /<<<DB>>>/{sub(/<<<DB>>>/, s)} 1' node_modules/mime-db/db.json Mime.hx > mime/Mime.hx
-zip -r mime.zip mime haxelib.json README.md -x "*/\.*"
+cp node_modules/mime-db/db.json mime-db.json
+zip -r mime.zip mime haxelib.json extraParams.hxml mime-db.json README.md -x "*/\.*"
 haxelib submit mime.zip
 rm mime.zip 2> /dev/null
