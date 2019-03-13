270a817b06c54b4d1690430ca538b56d7f730498
Ben Merckx <ben@codeurs.be>
2019-03-13 16:23:17 -0400

    Lixify / cache extensions lookup

--- submit.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-npm install mime-db --silent
-cp node_modules/mime-db/db.json mime-db.json
-zip -r mime.zip mime haxelib.json extraParams.hxml mime-db.json README.md -x "*/\.*"
-haxelib submit mime.zip
-rm mime.zip 2> /dev/null
